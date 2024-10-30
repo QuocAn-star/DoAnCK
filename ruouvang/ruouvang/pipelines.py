2172e7dd766257bcffdddaba4da8364db455239f
Thu Ngan <nttn05072004@gmail.com>
2024-10-30 09:03:53 +0700

    Chinh

--- ruouvang/ruouvang/pipelines.py
+++ ruouvang/ruouvang/pipelines.py
@@ -30,25 +30,24 @@ class RuouvangPipeline:
         )'''
         )
 
-        def process_item(self, item, spider):
-            self.cursor.execute('''
-            INSERT INTO wines(name, price, img, url, wine_type, grape_variety, alcohol_concentration, volume, vintage)
-            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)
-            ''',(
-                item.get('name'),
-                item.get('price'),
-                item.get('img'),
-                item.get('url'),
-                item.get('wine_type'),
-                item.get('grape_variety'),
-                item.get('alcohol_concentration'),
-                item.get('volume'),
-                item.get('vintage')
-            ))
 
-            return item
+    def process_item(self, item, spider):
+        self.cursor.execute('''
+        INSERT INTO wines(name, price, img, url, wine_type, grape_variety, alcohol_concentration, volume, vintage)
+        VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''',(
+            item.get('name'),
+            item.get('price'),
+            item.get('img'),
+            item.get('url'),
+            item.get('wine_type'),
+            item.get('grape_variety'),
+            item.get('alcohol_concentration'),
+            item.get('volume'),
+            item.get('vintage')
+        ))
+
+        return item
     def close_spider(self, spider):
         self.connection.commit()
         self.connection.close()
-
-
